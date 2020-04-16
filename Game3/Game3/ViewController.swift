5c3127c422d3a25d317f206b003be6a8b4e81fca
fabiandang <fabiandang@gmail.com>
2020-04-17 00:59:29 +0700

    //keyboard dismiss

--- Game3/Game3/ViewController.swift
+++ Game3/Game3/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+//keyboard dismiss
+extension UIViewController {
+    func HideKeyboard() {
+        
+        let Tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DismissKeyboard))
+               
+        view.addGestureRecognizer(Tap)
+        
+    }
+    
+    @objc func DismissKeyboard() {
+    
+        view.endEditing(true)
+    }
+    
+    
+}
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textNameInput: UITextField!
+    
+    @IBOutlet weak var textEmailInput: UITextField!
+    
+    @IBOutlet weak var textPasswordInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textNameInput.delegate = self
+        textEmailInput.delegate = self
+        textPasswordInput.delegate = self
+        
+        self.HideKeyboard()
+      
     }
-
-
+    
+    //keyboard return
+    func textFieldShouldReturn(_ textField:UITextField) -> Bool {
+        
+        textNameInput.resignFirstResponder()
+        textEmailInput.resignFirstResponder()
+        textPasswordInput.resignFirstResponder()
+        
+        return true
+        
+    }
+    
 }
 
