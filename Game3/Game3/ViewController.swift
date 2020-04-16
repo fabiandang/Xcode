d560432bd9517aa60f054d0f2b549b8393730750
fabiandang <fabiandang@gmail.com>
2020-04-16 17:11:11 +0700

    New project

---
+++ Game3/Game3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game3
+//
+//  Created by Temp on 4/16/20.
+//  Copyright © 2020 Fabi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
