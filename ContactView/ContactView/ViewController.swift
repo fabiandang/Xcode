b421fa1e48664274b77c94c9f4e07644c7981177
fabiandang <fabiandang@gmail.com>
2020-04-22 04:11:25 +0700

    add phonenumber + searchbar

--- ContactView/ContactView/ViewController.swift
+++ ContactView/ContactView/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var contactView: UITableView!
     
     var namesGroup = [String:[Contact]]()
     var keySectionTitles = [String]()
+    var filterArray: [Contact] = []
     let contacts:[Contact] = ContactDataProvider.contacts
     
     override func viewDidLoad() {
@@ -22,9 +24,14 @@ class ViewController: UIViewController {
       
         contactView.delegate = self
         contactView.dataSource = self
+        searchBar.delegate = self
         
-        for key in contacts {
-            
+        if searchBar.searchTextField.text?.isEmpty == true {
+            filterArray = contacts
+        }
+        
+        for key in filterArray {
+         
             let keysection = String(key.name.prefix(1))
             if var keyvalues = namesGroup[keysection] {
                 
@@ -52,7 +59,7 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("you taped \(contacts[indexPath.row])")
+        print("you taped \(filterArray[indexPath.row])")
     }
     
     
@@ -62,14 +69,16 @@ extension ViewController: UITableViewDelegate {
 extension ViewController: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
+
         return keySectionTitles.count
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         let keysection = keySectionTitles[section]
         if let keyvalues = namesGroup[keysection] {
-            
+ 
             return keyvalues.count
             
         }
@@ -80,13 +89,19 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
         
         let keysection = keySectionTitles[indexPath.section]
         
         if let keyvalues = namesGroup[keysection] {
+            
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+            
             let contact = keyvalues[indexPath.row]
+            
             cell.textLabel?.text = contact.name
+            
+            cell.detailTextLabel?.text = contact.phoneNumber
               
         }
         
@@ -99,4 +114,58 @@ extension ViewController: UITableViewDataSource {
         return keySectionTitles[section]
         
     }
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        
+        UIView.animate(withDuration: 0.3 ) {
+            
+            self.view.layoutIfNeeded()
+            
+        }
+        
+        contactView.isHidden = false
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        
+        
+        filterArray = contacts
+        namesGroup = [String:[Contact]]()
+        
+        if searchText.isEmpty == false {
+            
+            filterArray = contacts.filter({$0.name.contains(searchText)})
+            
+        }
+        
+        for key in filterArray {
+            
+            let keysection = String(key.name.prefix(1))
+
+           
+            if var keyvalues = namesGroup[keysection] {
+                
+                keyvalues.append(key)
+                namesGroup[keysection] = keyvalues
+
+            }
+            else {
+                
+                namesGroup[keysection] = [key]
+            }
+        
+        }
+        
+        keySectionTitles = [String](namesGroup.keys)
+        keySectionTitles = keySectionTitles.sorted(by: {$0 < $1})
+        
+        contactView.reloadData()
+   
+    }
+    
 }
