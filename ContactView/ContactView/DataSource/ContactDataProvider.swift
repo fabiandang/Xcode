9270388b417c77c432ca95e93bd78912ac8cc1ec
Luc Nguyen <nguyentienluc.mochoa@gmail.com>
2020-04-21 12:50:54 +0700

    Refactor the src code

--- ContactView/ContactView/DataSource/ContactDataProvider.swift
+++ ContactView/ContactView/DataSource/ContactDataProvider.swift
@@ -10,13 +10,13 @@ import Foundation
 
 struct ContactDataProvider {
     static let contacts: [Contact] = [
-        Contact("Alexander", "+1-541-754-3010"),
-        Contact("Ava", "+49-89-636-48018"),
-        Contact("Amelia", "+49-89-636-48019"),
-        Contact("Madison", "+1-642-754-3010"),
+        Contact("Alexander","+1-541-754-3010"),
+        Contact("Ava",      "+49-89-636-48018"),
+        Contact("Amelia",   "+49-89-636-48019"),
+        Contact("Madison",  "+1-642-754-3010"),
         Contact("Margaret", "+49-89-999-48018"),
-        Contact("Sophia", "+1-541-754-3010"),
+        Contact("Sophia",   "+1-541-754-3010"),
         Contact("Samantha", "+49-89-636-48018"),
-        Contact("Susan", "+49-89-636-48019")
+        Contact("Susan",    "+49-89-636-48019")
     ]
 }
